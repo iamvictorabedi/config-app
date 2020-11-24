94aaf4a2bffc557e4029a89ffd4422e4a33afe5d
Victor Abedi <v.nyangweso@carepay.co.ke>
2020-11-24 10:38:53 +0300

    Dev Initialize project and spring java configurations

---
+++ src/test/java/com/kyosk/config/ConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kyosk.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
